fb58ab38fffa52017ddd32b6cb98b2f87759576f
Grace <gl00233@surrey.ac.uk>
2021-02-24 17:52:12 +0000

    Added game states logic - waiting, playing, paused, and game over

--- PongGame.cs
+++ PongGame.cs
@@ -28,6 +28,12 @@ namespace SinglePlayerPong
         GamePadState pad1;
         KeyboardState keyb;
 
+        int gameState;
+        const int WAITING = 0, PLAYING = 1, PAUSED = 2, GAME_OVER = 3;
+
+        int lives;
+
+
         public PongGame()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -37,6 +43,8 @@ namespace SinglePlayerPong
 
         protected override void Initialize()
         {
+            gameState = WAITING;
+
             playAreaRectangle = new Rectangle(50, 40, 700, 400);
             playAreaColor = Color.Honeydew;
             blankTexture = new Texture2D(GraphicsDevice, 1, 1);
@@ -80,56 +88,86 @@ namespace SinglePlayerPong
             if (capabilities.IsConnected)
                 pad1 = GamePad.GetState(PlayerIndex.One);
 
-            if (keyb.IsKeyDown(Keys.Right) || keyb.IsKeyDown(Keys.D) || pad1.ThumbSticks.Left.X > 0)
-            {
-                paddleRectangle.X += paddleSpeed;
-                if (ballYSpeed == 0)
+            if (gameState != PLAYING)
+            { 
+                if (keyb.IsKeyDown(Keys.Space) || pad1.Buttons.Start == ButtonState.Pressed)
                 {
+                    if (gameState != PAUSED)
+                    {
+                        lives = 3;
+                    }
+
+                    if (gameState == GAME_OVER)
+                    {
+                        ballRectangle.Y = playAreaRectangle.Top;
+                        ballRectangle.X = (GraphicsDevice.Viewport.Width / 2) - (ballRectangle.Width / 2);
+                    }
+                    gameState = PLAYING;
                     ballXSpeed = playAreaRectangle.Width / 160;
                     ballYSpeed = playAreaRectangle.Width / 160;
                 }
             }
 
-            else if (keyb.IsKeyDown(Keys.Left) || keyb.IsKeyDown(Keys.A) || pad1.ThumbSticks.Left.X < 0)
+            if (gameState == PLAYING || gameState == PAUSED)
             {
-                paddleRectangle.X -= paddleSpeed;
-                if (ballYSpeed == 0)
+                if (keyb.IsKeyDown(Keys.Left) || keyb.IsKeyDown(Keys.A) || pad1.ThumbSticks.Left.X < 0)
                 {
-                    ballXSpeed = playAreaRectangle.Width / 160;
-                    ballYSpeed = playAreaRectangle.Width / 160;
+                    paddleRectangle.X -= paddleSpeed;
+                }
+
+                if (keyb.IsKeyDown(Keys.Right) || keyb.IsKeyDown(Keys.D) || pad1.ThumbSticks.Left.X > 0)
+                {
+                    paddleRectangle.X += paddleSpeed;
                 }
             }
 
-            if (ballYSpeed == 0 && (keyb.IsKeyDown(Keys.Escape) || pad1.Buttons.B == ButtonState.Pressed))
+            if ((gameState == WAITING || gameState == GAME_OVER) && (keyb.IsKeyDown(Keys.Escape) || pad1.Buttons.B == ButtonState.Pressed))
                 Exit();
 
-            if (paddleRectangle.Left < playAreaRectangle.Left)
-                paddleRectangle.X = playAreaRectangle.Left;
-            if (paddleRectangle.Right > playAreaRectangle.Right)
-                paddleRectangle.X = playAreaRectangle.Right - paddleRectangle.Width;
-
-            if (ballRectangle.Left < playAreaRectangle.Left)
-                ballRectangle.X = playAreaRectangle.Left;
-            if (ballRectangle.Right > playAreaRectangle.Right)
-                ballRectangle.X = playAreaRectangle.Right - ballRectangle.Width;
-            if (ballRectangle.Top < playAreaRectangle.Top)
-                ballRectangle.Y = playAreaRectangle.Top;
-            if (ballRectangle.Bottom > playAreaRectangle.Bottom)
-                ballRectangle.X = playAreaRectangle.Bottom - ballRectangle.Height;
-
-            if (ballRectangle.X == playAreaRectangle.Left || ballRectangle.X == (playAreaRectangle.Right - ballRectangle.Width))
-                ballXSpeed *= -1;
-
-            if (ballRectangle.Intersects(paddleRectangle) || ballRectangle.Y == playAreaRectangle.Top)
-                ballYSpeed *= -1;
-
-            if (ballRectangle.Bottom > playAreaRectangle.Bottom)
+            if (gameState == PLAYING)
             {
-                ballRectangle.Y = playAreaRectangle.Top;
-                ballRectangle.X = (GraphicsDevice.Viewport.Width / 2) - (ballRectangle.Width / 2);
-                ballYSpeed = 0;
-                ballXSpeed = 0;
-                paddleRectangle.X = (GraphicsDevice.Viewport.Width / 2) - (paddleRectangle.Width / 2);
+                if (paddleRectangle.Left < playAreaRectangle.Left)
+                    paddleRectangle.X = playAreaRectangle.Left;
+                if (paddleRectangle.Right > playAreaRectangle.Right)
+                    paddleRectangle.X = playAreaRectangle.Right - paddleRectangle.Width;
+
+                if (ballRectangle.Left < playAreaRectangle.Left)
+                    ballRectangle.X = playAreaRectangle.Left;
+                if (ballRectangle.Right > playAreaRectangle.Right)
+                    ballRectangle.X = playAreaRectangle.Right - ballRectangle.Width;
+                if (ballRectangle.Top < playAreaRectangle.Top)
+                    ballRectangle.Y = playAreaRectangle.Top;
+
+                if (ballRectangle.X == playAreaRectangle.Left || ballRectangle.X == (playAreaRectangle.Right - ballRectangle.Width))
+                    ballXSpeed *= -1;
+
+                if (ballRectangle.Y == playAreaRectangle.Top)
+                    ballYSpeed *= -1;
+
+                if (ballRectangle.Intersects(paddleRectangle))
+                {
+                    ballYSpeed *= -1;
+                    ballRectangle.Y = paddleRectangle.Top - ballRectangle.Height;
+                }
+
+                if (ballRectangle.Bottom > playAreaRectangle.Bottom)
+                {
+                    if (lives > 1)
+                    {
+                        gameState = PAUSED;
+                        ballRectangle.Y = playAreaRectangle.Top;
+                        ballRectangle.X = (GraphicsDevice.Viewport.Width / 2) - (ballRectangle.Width / 2);
+                        ballYSpeed = 0;
+                        ballXSpeed = 0;
+                        lives -= 1;
+                    }
+                    else
+                    {
+                        gameState = GAME_OVER;
+                        ballYSpeed = 0;
+                        ballXSpeed = 0;
+                    }
+                }
             }
 
 
@@ -145,9 +183,16 @@ namespace SinglePlayerPong
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(blankTexture, playAreaRectangle, playAreaColor);
-            _spriteBatch.Draw(ballTexture, ballRectangle, ballColor);
             _spriteBatch.Draw(paddleTexture, paddleRectangle, paddleColor);
+
+            if (gameState == PLAYING || gameState == GAME_OVER)
+            {
+                _spriteBatch.Draw(ballTexture, ballRectangle, ballColor);
+            }
+
             _spriteBatch.End();
+
+
 
             base.Draw(gameTime);
         }
